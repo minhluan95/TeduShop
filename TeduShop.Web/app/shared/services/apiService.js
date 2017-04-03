6974494dfa1ea57297db8be25077053ab9f67869
MINHLUANPC\Minh Luan <cuminhluan1995>
2017-04-03 14:31:58 +0700

    add update product category

--- TeduShop.Web/app/shared/services/apiService.js
+++ TeduShop.Web/app/shared/services/apiService.js
@@ -6,10 +6,13 @@
     apiService.$inject = ['$http', 'notificationService'];
 
     function apiService($http, notificationService) {
+
         return {
             get: get,
-            post: post
+            post: post,
+            put: put
         }
+
         function post(url, data, success, failure) {
             $http.post(url, data).then(function (result) {
                 success(result);
@@ -24,6 +27,22 @@
 
             });
         }
+
+        function put(url, data, success, failure) {
+            $http.put(url, data).then(function (result) {
+                success(result);
+            }, function (error) {
+                console.log(error.status)
+                if (error.status === 401) {
+                    notificationService.displayError('Authenticate is required.');
+                }
+                else if (failure != null) {
+                    failure(error);
+                }
+
+            });
+        }
+
         function get(url, params, success, failure) {
             $http.get(url, params).then(function (result) {
                 success(result);
@@ -32,4 +51,5 @@
             });
         }
     }
+
 })(angular.module('tedushop.common'));
